025ef5ba19e98be68389dc53c99633152a2a56a5
divyanshu <dmahi@gitam.in>
2024-12-14 23:07:54 +0530

    Remove 'Feedback' option from navbar: index.html, info.html, directContact.html, share.html.
    Remove 'Horoscope' button: index.html.
    
    Rename 'container3' buttons names: index.html
    
    Refactor contents of div; id="contact-info": directContact.html
    
    Move script to copy url from index.js into the share.html
    
    Create JS Libraries to display data on webpage from Cloud Firestore: firebaseConfig.js, firebaseFunctions.js, directContact.js, info.js,
    
    Add comments: style.css
    
    Remove the "Horoscope" button styles: style.css

--- www/Libs/LiveDB/firebaseLiveDB.js
+++ www/Libs/LiveDB/firebaseLiveDB.js
@@ -1,20 +1,6 @@
-//Firebase live database:
+import { app } from "../firebaseConfig.js"
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDEOWqRAqiVgVuZNw5-Qyd0emw4hLH6WOg",
-    authDomain: "name-bazaar-34aef.firebaseapp.com",
-    databaseURL: "https://name-bazaar-34aef-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "name-bazaar-34aef",
-    storageBucket: "name-bazaar-34aef.firebasestorage.app",
-    messagingSenderId: "597127145024",
-    appId: "1:597127145024:web:d52929fb19a9dc46fa21a5"
-};
-
-
-const app = initializeApp(firebaseConfig);
 
 const contactFormDB = getDatabase(app);
 
