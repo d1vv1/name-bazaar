c0dfeb7eeb98de45c3da2ca4ea082d217d2cc059
divyanshu <dmahi@gitam.in>
2024-12-21 13:18:47 +0530

    Style app for mobile devices

--- www/JavaScript/index.js
+++ www/JavaScript/index.js
@@ -1,9 +1,24 @@
 function showSidebar(bar) {
     const sidebar = document.querySelector("." + bar)
+
     sidebar.style.display = "flex";
+
+    if(bar === "sidebar") {
+        const otherBar = "l-sidebar";
+        hideSidebar(otherBar);
+    }
+    else if(bar === "l-sidebar") {
+        const otherBar = "sidebar";
+        hideSidebar(otherBar);
+    }
 }
 
 function hideSidebar(bar){
     const sidebar = document.querySelector("." + bar)
+
     sidebar.style.display = "none";
 }
+
+const mediaQuery = window.matchMedia('(min-width: 826px)');
+
+mediaQuery.addEventListener('change', () => hideSidebar("sidebar"));
