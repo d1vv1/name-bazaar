03eb9a5f75445aedd82e1bab0027470088cd404d
divyanshu <dmahi@gitam.in>
2024-12-29 19:43:48 +0530

    Edit style for deploy

--- www/Libs/CloudFirestore/directContact.js
+++ www/Libs/CloudFirestore/directContact.js
@@ -1,6 +1,7 @@
 import { displayTextOnPage } from "../firebaseFunctions.js";
 
 displayTextOnPage("data", "Contact Us", "Phone Nos", "phone-number");
+displayTextOnPage("data", "Contact Us", "email", "mail-address");
 displayTextOnPage("data", "Contact Us", "english/blueheading1", "bh1");
 displayTextOnPage("data", "Contact Us", "english/blueheading2", "bh2");
 displayTextOnPage("data", "Contact Us", "english/para1", "para1");
