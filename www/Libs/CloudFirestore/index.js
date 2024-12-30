6a7ac716f4879cc35209a2e9a448366b9464a2a0
divyanshu <dmahi@gitam.in>
2024-12-30 22:13:08 +0530

    Edit index Style

--- www/Libs/CloudFirestore/index.js
+++ www/Libs/CloudFirestore/index.js
@@ -1,12 +1,12 @@
 import { displayTextOnPage, loadRandomImage, loadImage } from '../firebaseFunctions.js'
 
-// loadRandomImage('im1')
-// loadRandomImage('im2')
-// loadRandomImage('info')
 
 loadImage("images", "image1", "path", "im1")
 loadImage("images", "image2", "path", "im2")
 loadImage("images", "image3", "path", "info")
 loadImage("images", "image4", "path", "other-info")
 
-displayTextOnPage('data', "services", "homePageInfo", "text");
+displayTextOnPage('data', "services", "homePageInfo/pi1", "text");
+displayTextOnPage('data', "services", "homePageInfo/pi2", "text");
+displayTextOnPage('data', "services", "homePageInfo/pi3", "text");
+displayTextOnPage('data', "services", "homePageInfo/pi4", "text");
