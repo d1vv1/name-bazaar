aecf61afc9c6494c3d2ad3258350a717cbf580db
divyanshu <dmahi@gitam.in>
2025-01-01 22:58:52 +0530

    Add more backend functionality

--- www/Libs/CloudFirestore/index.js
+++ www/Libs/CloudFirestore/index.js
@@ -1,12 +1,13 @@
-import { displayTextOnPage, loadRandomImage, loadImage } from '../firebaseFunctions.js'
+import { displayTextOnPage, loadImage } from '../firebaseFunctions.js'
 
+await displayTextOnPage('data', "services", "homePageInfo/homePageHeading", "home-head");
+await displayTextOnPage('data', "services", "homePageInfo/boxHead", "box-head");
+await displayTextOnPage('data', "services", "homePageInfo/pi1", "text");
+await displayTextOnPage('data', "services", "homePageInfo/pi2", "text");
+await displayTextOnPage('data', "services", "homePageInfo/pi3", "text");
+await displayTextOnPage('data', "services", "homePageInfo/pi4", "text");
 
-loadImage("images", "image1", "path", "im1")
-loadImage("images", "image2", "path", "im2")
-loadImage("images", "image3", "path", "info")
-loadImage("images", "image4", "path", "other-info")
-
-displayTextOnPage('data', "services", "homePageInfo/pi1", "text");
-displayTextOnPage('data', "services", "homePageInfo/pi2", "text");
-displayTextOnPage('data', "services", "homePageInfo/pi3", "text");
-displayTextOnPage('data', "services", "homePageInfo/pi4", "text");
+await loadImage("images", "image1", "path", "im1")
+await loadImage("images", "image2", "path", "im2")
+await loadImage("images", "image3", "path", "info")
+await loadImage("images", "image4", "path", "other-info")
--- www/Libs/CloudFirestore/info.js
+++ www/Libs/CloudFirestore/info.js
@@ -1,3 +1,9 @@
 import { displayTextOnPage } from '../firebaseFunctions.js'
 
-displayTextOnPage("data", "Information", "english", "info-text-p")
+displayTextOnPage("data", "Web info", "heading", "info-head");
+
+displayTextOnPage("data", "Web info", "para1(e)", "info-e");
+displayTextOnPage("data", "Web info", "para1(hindi)", "info-h");
+displayTextOnPage("data", "Web info", "alertText", "alert-txt");
+displayTextOnPage("data", "Web info", "serveList", "serve-head");
+displayTextOnPage("data", "Web info", "serveFields", "serve-lst");
